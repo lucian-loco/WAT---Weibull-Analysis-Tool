aebc7d8506d4d8464b1c76b043e26861c16f93e7
Maciej Suminski <maciej.suminski@cern.ch>
2024-04-25 15:52:12 +0200

    Set temp directories paths

--- gunicorn_starter.sh
+++ gunicorn_starter.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-gunicorn  --worker-tmp-dir /dev/shm --chdir app -w 2 --threads 2 -b 0.0.0.0:8888 main:app
+# TMP_DIR="$(mktemp -d)"
+TMP_DIR="/tmp/weibull-cache"
+mkdir -p "$TMP_DIR"
+export XDG_CACHE_HOME="$TMP_DIR"
+export MPLCONFIGDIR="$TMP_DIR"
+gunicorn --worker-tmp-dir /dev/shm --chdir app -w 2 --threads 2 -b 0.0.0.0:8888 main:app
