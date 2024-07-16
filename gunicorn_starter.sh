e05f561d3d0125bb23df9fd998ded7910404c04e
Maciej Suminski <maciej.suminski@cern.ch>
2024-07-16 10:51:57 +0200

    Moved some gunicorn settings to env variables

--- gunicorn_starter.sh
+++ gunicorn_starter.sh
@@ -3,9 +3,31 @@ if [ -z "$DB_USER" ] || [ -z "$DB_PASS" ] || [ -z "$DB_HOST" ] || [ -z "$DB_PORT
     echo "WARNING: Missing database environment variables (one of: DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_SERV)."
 fi
 
+# Default gunicorn settings
+if [ -z "$GUNICORN_WORKERS" ]; then
+    GUNICORN_WORKERS=2
+fi
+
+
+if [ -z "$GUNICORN_THREADS" ]; then
+    GUNICORN_THREADS=2
+fi
+
+if [ -z "$GUNICORN_TIMEOUT" ]; then
+    GUNICORN_TIMEOUT=45
+fi
+
+if [ -z "$GUNICORN_LOG_LEVEL" ]; then
+    GUNICORN_LOG_LEVEL="info"
+fi
+
+
 # TMP_DIR="$(mktemp -d)"
 TMP_DIR="/tmp/hit-data-cache"
 mkdir -p "$TMP_DIR"
 export XDG_CACHE_HOME="$TMP_DIR"
 export MPLCONFIGDIR="$TMP_DIR"
-gunicorn --worker-tmp-dir /dev/shm --chdir src -w 2 --threads 2 -b 0.0.0.0:8888 main:app
+gunicorn --worker-tmp-dir /dev/shm --chdir src \
+    --log-level $GUNICORN_LOG_LEVEL --workers $GUNICORN_WORKERS \
+    --threads $GUNICORN_THREADS --timeout $GUNICORN_TIMEOUT \
+    -b 0.0.0.0:8888 main:app
