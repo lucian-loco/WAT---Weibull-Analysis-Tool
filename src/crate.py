32b7adba4e9f93969a857154a52e3c2aaacfee9b
Maciej Suminski <maciej.suminski@cern.ch>
2024-07-17 11:00:35 +0200

    crate: Acquire data from LayoutDB

--- src/crate.py
+++ src/crate.py
@@ -5,20 +5,68 @@ import io
 import glob
 import os
 import copy
+import functools
+from enum import Enum
 import drawio
+import db_hitdata
 import shapelinks
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+class Orientation(Enum):
+    HORIZONTAL = 1
+    VERTICAL = 2
+
+
+class Face(Enum):
+    FRONT = 1
+    BACK = 2
+
+
+@dataclass
+class Dimension:
+    start: float
+    end: float
+
+
+    def __post_init__(self):
+        assert(self.start <= self.end)
+
+
+    @property
+    def length(self):
+        return self.end - self.start
+
+
+    def intersects(self, value):
+        return self.start <= value and value <= self.end
 
 
 @dataclass
 class Module:
+    position: str   # EAM position
     typeName: str
     typeCode: str
     slotNumber: int
     lun: int
+    width: Dimension = None
+    height: Dimension = None
+    depth: Dimension = None
+
+
+    @property
+    def orientation(self):
+        if self.width.length > self.height.length:
+            return Orientation.HORIZONTAL
+        else:
+            return Orientation.VERTICAL
 
 
 @dataclass
 class Crate:
+    position: str   # EAM position
     name: str
     typeName: str
     typeCode: str
@@ -26,7 +74,16 @@ class Crate:
     rackName: str
     modules: list[Module]
 
+    width: float = None     # width in meters
+    height: float = None    # height in meters
+    depth: float = None     # depth in meters
+    widthDim: int = None    # number of uniform slots in the width dimension
+    heightDim: int = None   # number of uniform slots in the height dimension
+    depthDim: int = None    # number of uniform slots in the depth dimension
+
+
     def slot(self, index):
+        """ Returns the module in the specified slot. """
         for m in self.modules:
             if m.slotNumber == index:
                 return m
@@ -34,32 +91,119 @@ class Crate:
         return None
 
 
-@dataclass
-class CrateLink:
-    libraryName: str
-    shapeName: str
-    slotWidth: int
-    firstSlotX: int
-    firstSlotY: int
+    def is_front_module(self, module: Module):
+        """ Checks if the module is located at the front of the crate. """
+        # the high depth position indicates the front
+        return module.depth.intersects(self.depthDim)
 
 
-def get_ccde_crate(crate):
-    crate_data = ccda.crate_by_label(crate)
+    def is_back_module(self, module: Module):
+        """ Checks if the module is located at the back of the crate. """
+        # the low depth position indicates the back
+        return module.depth.intersects(1)
+
+
+    def is_module_face(self, module: Module, face: Face):
+        if face == Face.FRONT:
+            return self.is_front_module(module)
+        elif face == Face.BACK:
+            return self.is_back_module(module)
+        else:
+            raise ValueError('Invalid face')
+
+
+@functools.lru_cache(32)
+def get_crate_layout_data(equipment_code):
+    cursor = db_hitdata.get_cursor()
+    query = f"SELECT WIDTH, HEIGHT, DEPTH, WIDTH_DIM, HEIGHT_DIM, DEPTH_DIM " \
+                "FROM crate_dimensions_v WHERE EQUIPMENT_CODE = :equipment_code"
+    response = cursor.execute(query, (equipment_code,)).fetchall()
+
+    if len(response) != 1:
+        raise ValueError(f'Invalid equipment code: {equipment_code}')
+
+    return {
+        'width': response[0][0],
+        'height': response[0][1],
+        'depth': response[0][2],
+        'width_dim': response[0][3],
+        'height_dim': response[0][4],
+        'depth_dim': response[0][5]
+    }
+
+
+def get_module_layout_data(position):
+    cursor = db_hitdata.get_cursor()
+    # TODO MODULE_DESCRIPTION? MODULE_EQUIPMENT_CODE?
+    query = f"SELECT SLOT_NUMBER, " \
+            "H_START_NUM, H_END_NUM, " \
+            "W_START_NUM, W_END_NUM, " \
+            "D_START_NUM, D_END_NUM " \
+            "FROM module_positions_v WHERE MODULE_NAME = :position"
+    response = cursor.execute(query, (position,)).fetchall()
+
+    if len(response) != 1:
+        raise ValueError(f'Invalid module position: {position}')
+
+    return {
+        'slot_number': response[0][0],
+        'height': Dimension(response[0][1], response[0][2]),
+        'width': Dimension(response[0][3], response[0][4]),
+        'depth': Dimension(response[0][5], response[0][6]),
+    }
+
+
+def make_crate(crate):
+    crate_ccde_data = ccda.crate_by_label(crate)
+
+    # Remove 'HC' prefix from the equipment code
+    assert(crate_ccde_data['typeCode'].startswith('HC'))
+    crate_ccde_data['typeCode'] = crate_ccde_data['typeCode'][2:]
+
+    crate_layout_data = get_crate_layout_data(crate_ccde_data['typeCode'])
 
     crate = Crate(
-            name         = crate_data['label'],
-            typeName     = crate_data['typeName'],
-            typeCode     = crate_data['typeCode'],
-            locationName = crate_data['locationName'],
-            rackName     = crate_data['rackName'],
+            position     = crate_ccde_data['name'],
+            name         = crate_ccde_data['label'],
+            typeName     = crate_ccde_data['typeName'],
+            typeCode     = crate_ccde_data['typeCode'],
+            locationName = crate_ccde_data['locationName'],
+            rackName     = crate_ccde_data['rackName'],
+
+            width        = crate_layout_data['width'],
+            height       = crate_layout_data['height'],
+            depth        = crate_layout_data['depth'],
+            widthDim     = crate_layout_data['width_dim'],
+            heightDim    = crate_layout_data['height_dim'],
+            depthDim     = crate_layout_data['depth_dim'],
+
             modules      = [])
 
-    for module_data in crate_data['modules']:
-        crate.modules.append(Module(
-            typeName   = module_data['typeName'],
-            typeCode   = module_data['typeCode'],
-            slotNumber = module_data['slotNumber'],
-            lun        = module_data['lun']))
+    for module_ccde_data in crate_ccde_data['modules']:
+        # Remove 'HC' prefix from the equipment code
+        assert(module_ccde_data['typeCode'].startswith('HC'))
+        module_ccde_data['typeCode'] = module_ccde_data['typeCode'][2:]
+
+        module = Module(
+            position   = module_ccde_data['name'],
+            typeName   = module_ccde_data['typeName'],
+            typeCode   = module_ccde_data['typeCode'],
+            slotNumber = module_ccde_data['slotNumber'],
+            lun        = module_ccde_data['lun'])
+
+        try:
+            module_layout_data = get_module_layout_data(module_ccde_data['name'])
+            module.width      = module_layout_data['width']
+            module.height     = module_layout_data['height']
+            module.depth      = module_layout_data['depth']
+
+            if module_ccde_data['slotNumber'] != module_layout_data['slot_number']:
+                logger.warning('Slot number mismatch for %s (CCDE:%d / Layout:%d)',
+                                module_ccde_data['name'], module_ccde_data['slotNumber'], module_layout_data['slot_number'])
+        except ValueError as e:
+            logger.warning('Could not find module layout data for %s', module_ccde_data['name'])
+
+        crate.modules.append(module)
 
     return crate
 
@@ -74,7 +218,7 @@ def generate_graph(crate):
     SHAPE_OFFSET_X = -72
     SHAPE_OFFSET_Y = 72
 
-    crate = get_ccde_crate(crate)
+    crate = make_crate(crate)
 
     generator.clear_page(320, 240)
     generator.add_box(10, 200, 200, 20, text=crate.name)
@@ -118,12 +262,17 @@ for filepath in glob.glob(os.path.join('static/drawio', '*.xml')):
     try:
         generator.load_library(filepath)
     except:
-        print(f'ERROR: Could not load library {filepath}')
+        logger.error(f'Could not load library {filepath}')
 
 
 if __name__ == '__main__':
-    #crate = get_ccde_crate('cfv-193-ascool')
-    #print(crate)
+    import pprint
 
-    graph = generate_graph('cfv-774-cdv03')
-    print(graph.readlines())
+    #data = get_crate_layout_data('CVREC')
+    #print(data)
+
+    crate = make_crate('cfc-774-cdv35')
+    pprint.pprint(crate)
+
+    #graph = generate_graph('cfv-774-cdv03')
+    #print(graph.readlines())
