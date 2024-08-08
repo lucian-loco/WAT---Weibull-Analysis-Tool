2be61130d3b7ee488b25e2648741340c8b060b3e
Maciej Suminski <maciej.suminski@cern.ch>
2024-08-08 14:26:15 +0200

    db_hitdata: Check the connection state before creating a cursor

--- src/db_hitdata.py
+++ src/db_hitdata.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 import oracledb
 import os
+import logging
+
+logging.basicConfig(format='%(levelname)s:%(name)s:%(message)s')
+logger = logging.getLogger(__name__)
 
 _db_handle = None
 
 def get_cursor():
     global _db_handle
 
-    if _db_handle is None:
+    if _db_handle is None or not _db_handle.is_healthy():
         conn_params = {
             'user':         os.environ['DB_USER'],
             'password':     os.environ['DB_PASS'],
@@ -16,6 +20,7 @@ def get_cursor():
             'service_name': os.environ['DB_SERV'],
         }
 
+        logger.info('Connecting to the database')
         _db_handle = oracledb.connect(**conn_params)
 
     return _db_handle.cursor()
