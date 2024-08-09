2aad439cb109ad4c8b73f837b69be08986b7fd9d
Maciej Suminski <maciej.suminski@cern.ch>
2024-08-09 16:09:26 +0200

    drawio: automaticall load HIT shape libraries in the editor

--- src/main.py
+++ src/main.py
@@ -1,9 +1,29 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 import weibull
 import crate
-from flask import Flask, render_template, request, send_file
+import os
+import urllib.parse
+import functools
+from flask import Flask, render_template, request, send_file, url_for
 
 app = Flask(__name__)
+
+
+# TODO not very elegant, think of a better way
+@functools.cache
+def get_drawio_lib_urls():
+    """ Generate a list of draw.io libraries URL, encoded for use in draw.io editor """
+    drawio_lib_urls = []
+    drawio_lib_path = 'static/drawio'
+    base_url = url_for('static', filename=f'drawio', _external=True)
+
+    for filename in os.listdir(drawio_lib_path):
+        if os.path.isfile(os.path.join(drawio_lib_path, filename)):
+            url = f'{base_url}/{filename}'
+            encoded = urllib.parse.quote(url, safe='')
+            drawio_lib_urls.append(encoded)
+
+    return drawio_lib_urls
 
 
 @app.route('/')
@@ -33,11 +53,12 @@ def route_crate_edit():
     try:
         crate_name = request.args.get('name')
         graph = crate.generate_graph(crate_name)
+        drawio_libs = get_drawio_lib_urls()
     except RuntimeError as e:
         return 'Crate layout cannot be generated: ' + str(e), 400
 
     document = graph.getvalue().decode('utf-8')
-    return render_template('drawio.html', document_data=document)
+    return render_template('drawio.html', document_data=document, drawio_libs=drawio_libs)
 
 
 @app.route('/crate/get', methods=['GET'])
