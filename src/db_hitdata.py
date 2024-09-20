8f2fcb6ebb0137514818b67d13616d69d26444e3
Maciej Suminski <maciej.suminski@cern.ch>
2024-09-20 08:29:30 +0200

    db_hitdata: Reconnect to the database on each request (INST-6946)
    I am unable to set up the connection pool correctly. Sooner or later, it
    ends up with some stale connections, resulting in errors.

--- src/db_hitdata.py
+++ src/db_hitdata.py
@@ -6,28 +6,14 @@ import logging
 logging.basicConfig(format='%(levelname)s:%(name)s:%(message)s')
 logger = logging.getLogger(__name__)
 
-try:
-    __pool_size = os.environ['GUNICORN_THREADS'] + 1
-except KeyError:
-    __pool_size = 2
-
-__pool = oracledb.create_pool(
-    user=os.environ['DB_USER'],
-    password=os.environ['DB_PASS'],
-    host=os.environ['DB_HOST'],
-    port=os.environ['DB_PORT'],
-    service_name=os.environ['DB_SERV'],
-    min=__pool_size,
-    max=__pool_size,
-    increment=0,
-    timeout=60,
-    max_lifetime_session=5*60
-)
-
-logger.info('Created HIT database connection pool (size=%d)', __pool_size)
-
+__db_conn_params = {
+    'user':         os.environ['DB_USER'],
+    'password':     os.environ['DB_PASS'],
+    'host':         os.environ['DB_HOST'],
+    'port':         os.environ['DB_PORT'],
+    'service_name': os.environ['DB_SERV'],
+}
 
 def get_cursor():
-    global __pool
-    conn = __pool.acquire()
-    return conn.cursor()
+    db_handle = oracledb.connect(**__db_conn_params)
+    return db_handle.cursor()
