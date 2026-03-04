b266505c2e0dea01c2623269c4e1e6e8e5ca591c
Maciej Suminski <maciej.suminski@cern.ch>
2026-03-04 16:05:37 +0100

    main.sh: removed Xvfb from startup

--- main.sh
+++ main.sh
@@ -3,6 +3,5 @@ export ELECTRON_DISABLE_SECURITY_WARNINGS="true"
 export ELECTRON_ENABLE_LOGGING="false"
 export DISPLAY=":99"
 
-Xvfb $DISPLAY -screen 0 1024x768x16 -nolisten unix &
 ./update_drawio.sh
 ./gunicorn_starter.sh
